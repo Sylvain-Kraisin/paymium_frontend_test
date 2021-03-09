111356630bcd896b1e7c1d24c43d84b7527fbf63
Sylvain Kraisin <sylvain.kraisin@gmail.com>
2021-03-09 17:38:13 +0100

    trying to make a get requested with given url but there is a CORS policy issue

--- src/Transaction.js
+++ src/Transaction.js
@@ -1,10 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 
 function Transaction() {
+    const [data, setData] = useState([]);
 
-    return (
-        <div className="Transaction">
-        </div>
-    );
+    useEffect(() => {
+        const url = 'http://private-3f9656-paymiuminterviews.apiary-mock.com/transactions'
+
+        axios.get(url)
+            .then(res => setData(res.data))
+
+    }, []);
+
+    return (data)
 }
 
 export default Transaction;
